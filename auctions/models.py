27fa9079d5d7485e7399510d99890f77afff35a2
Huffman <chuffm01@kenvue.com>
2024-05-25 20:42:51 -0400

    Trying to get image upload working

--- auctions/models.py
+++ auctions/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(blank = True)
+    image = models.URLField(blank = True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     current_bid = models.DecimalField(max_digits=11, decimal_places=2, default=0.0)
     open = models.BooleanField(default=True)
